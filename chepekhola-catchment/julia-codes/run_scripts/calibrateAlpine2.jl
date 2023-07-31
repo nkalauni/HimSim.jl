660f376442d27ae7270e34f13bf91fccac1e4a4d
Nabin-Kalauni <075bce083.nabin@pcampus.edu.np>
2023-07-31 08:34:15 +0545

    Fix bugs in calibrateAlpine2

--- chepekhola-catchment/julia-codes/run_scripts/calibrateAlpine2.jl
+++ chepekhola-catchment/julia-codes/run_scripts/calibrateAlpine2.jl
@@ -4,11 +4,11 @@ using OrdinaryDiffEq
 using Plots
 using DiffEqParamEstim
 using Optimization
-using OptimizationOptimJL
-using ForwardDiff
-using SciMLSensitivity 
-using OptimizationPolyalgorithms
-using Zygote
+# using OptimizationOptimJL
+# using ForwardDiff
+# using SciMLSensitivity 
+# using OptimizationPolyalgorithms
+# using Zygote
 using OptimizationCMAEvolutionStrategy
 ### Temporary
 using CSV
@@ -16,8 +16,10 @@ using Dates
 using DataFrames
 
 include("../src/tools/utils.jl")
+
+const SMOOTHER = 0.01
  
-@parameters Tt ddf Smax Sfc tcin tcbf
+@parameters Tt ddf Smax cfc tcin tcbf
  
 @variables t Q(t) Qt(t) Qsc(t) Qin(t) Qbf(t) Pr(t) Ps(t) QN(t) Ea(t) S(t) Sn(t)
    
@@ -43,19 +45,26 @@ end
  
 @register_symbolic excess(check, limit, default, alternate)
  
+x = collect(LinRange(1.0, 10.0, 50))
+plot(x, 10.0 * (1 ./ (1 .+ exp.((x .- 5.0) / SMOOTHER))))
+plot(x, 10.0 * (1 .- (1 ./ (1 .+ exp.((x .- 5.0) / SMOOTHER)))))
+
 eqn = [D(Sn) ~ Ps - QN,
-    Ps ~ excess(Tt, T(t), P(t), zero(P(t))),
+    # Ps ~ excess(Tt, T(t), P(t), zero(P(t))),
+    Ps ~ P(t) * (1 / (1 + exp((T(t) - Tt) / SMOOTHER))),
     QN ~ max(min(ddf * (T(t) - Tt), Sn), 0),
     # QN ~ max(T(t) - Tt, zero(T(t))) * ddf,
     D(S) ~ Pr + QN - Ea - Qsc - Qin - Qbf,
-    Pr ~ excess(T(t), Tt, P(t), zero(P(t))),
-    Ea ~ excess(S, zero(S), Ep(t), zero(Ep(t))),
-    Qsc ~ excess(S, Smax, Pr + QN, zero(Pr)),
-    Qin ~ max(S - Sfc, zero(S)) * tcin,
+    Pr ~ P(t) * (1 - (1 / (1 + exp((T(t) - Tt) / SMOOTHER)))),
+    # Pr ~ excess(T(t), Tt, P(t), zero(P(t))),
+    # Ea ~ excess(S, zero(S), Ep(t), zero(Ep(t))),
+    Ea ~ min(S, Ep(t)),
+    Qsc ~ excess(S, Smax, P(t) + QN, zero(Pr)),
+    Qin ~ max(S - cfc * Smax, zero(S)) * tcin,
     Qbf ~ tcbf * S,
     Q ~ Qsc + Qin + Qbf]
  
-@named Alpine2 = ODESystem(eqn, t, [Sn, S, Pr, Ea, QN], [Tt, ddf, Smax, Sfc, tcin, tcbf])
+@named Alpine2 = ODESystem(eqn, t, [Sn, S, Pr, Ea, QN], [Tt, ddf, Smax, cfc, tcin, tcbf])
  
 model = Alpine2
 scr = structural_simplify(model)
@@ -63,21 +72,21 @@ equations(scr)
  
 u0 = [0.0, 0.0]
 tspan = (0.0, 3 * 365.0)
-param = [15.0, .5, 1000.0, 1.0, 0.5, 0.5]
+param = [15.0, .5, 1000.0, 0.95, 0.5, 0.5]
  
 t = collect(range(1.0, stop=tspan[2], length=1095))
  
 prob = ODEProblem(scr, u0, tspan, param)
-sol = solve(prob, Rodas5(), maxiters = 100_000_00, verbose = true)  # maxiters (solver??)
+sol = solve(prob, Rodas5(), maxiters = 100_000, verbose = true)  # maxiters (solver??)
  
 ######################
 # calibration attempt
 ######################
  
-cost_function = build_loss_objective(prob, Rodas5(), L2Loss(t, obs[1:1095]), Optimization.AutoForwardDiff(), maxiters=100_000_00, verbose=true)
+cost_function = build_loss_objective(prob, Rodas5(), L2Loss(t, obs[1:1095]), Optimization.AutoForwardDiff(), maxiters=100_000, verbose=true)
  
 optProbs = Optimization.OptimizationProblem(cost_function, param)
-optsol = solve(optProbs, NelderMead(), verbose = true) 
+optsol = solve(optProbs, CMAEvolutionStrategyOpt()) 
  
  
  
